7a09a1c6afc3f5889c32b6c2123c26302a690957
MikkioBoss <160290026+Mikkioboss@users.noreply.github.com>
2026-06-23 14:18:42 -0500

    Update need_to_add.py

--- need_to_add.py
+++ need_to_add.py
@@ -1,1 +1,5 @@
 sed -i "s/if 'GOOD' in d\['status'\] or 'COMPLETE' in d\['status'\]:/if d['lat'] != 0.0:/" /home/ncalm/novatel_recorder.py
+
+
+
+python3 /home/ncalm/novatel_recorder.py --receiver_id 0 --port /dev/ttyUSB1 --baud 115200 --output /home/ncalm/flight_data --no_camera
